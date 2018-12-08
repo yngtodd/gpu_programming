224977412cd912ed98fedb364fc612ae2f256e91
yngtodd <young.todd.mk@gmail.com>
2018-12-08 17:39:08 -0500

    spacing

--- src/cuda/fundamentals/saxpy.cu
+++ src/cuda/fundamentals/saxpy.cu
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-__global__
-void saxpy(int n, float, a, float *x, float *y)
+__global__ void saxpy(int n, float, a, float *x, float *y)
 {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int (i < n) y[i] = a * x[i] + y[i];
